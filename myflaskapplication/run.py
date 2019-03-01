b3ca3d000ae26b2966afaf1d122e049ff4b535a5
Neeraj Kumar <smartneeraj.j23@gmail.com>
2019-03-01 22:17:09 +0530

    Update run.py

--- myflaskapplication/run.py
+++ myflaskapplication/run.py
@@ -1,9 +1,4 @@
 from flaskblog import app
 if __name__== '__main__':
-<<<<<<< HEAD
-    #app.run(host='10.236.220.127',port=5111, debug=True)
     app.run(debug=True,host='0.0.0.0', threaded=True)
-=======
-    app.run(debug=True,host='0.0.0.0')
->>>>>>> 551df524904f1b984fa6890c220f0eb84de8db5a
 
